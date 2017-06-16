2dec3ea19df00cc792af491d647903b57b95230a
OwenHerndon <owen.w.herndon@gmail.com>
2017-06-16 11:13:42 -0700

    need to update db

--- DnDSpellBook/DnDSpellBook/app/Controllers/addCharacterController.js
+++ DnDSpellBook/DnDSpellBook/app/Controllers/addCharacterController.js
@@ -4,18 +4,7 @@
     $scope.newCharacterName = "";
 
     $scope.addCharacter = function () {
-        $http({
-            method: 'POST',
-            url: "/api/character/new",
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            transformRequest: function (obj) {
-                var str = [];
-                for (var p in obj)
-                    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-                return str.join("&");
-            },
-            data: { name: $scope.newCharacterName }
-        })
+        $http.post("/api/character/new", {name:$scope.newCharacterName})
             .then(function (result) {
                 console.log("result=", result);
                 $location.path("/character");
